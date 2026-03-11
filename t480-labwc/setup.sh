facdfd3e48184999af076f28446f47313089bc27
eliaaile <eliacortesi@etik.com>
2026-03-11 23:12:28 +0100

    chore: clean up config files
    Add output messages in `setup.sh' script.
    Clean up some packages and instructions.
    Fix stow section

--- t480-labwc/setup.sh
+++ t480-labwc/setup.sh
@@ -1,34 +1,56 @@
 #!/bin/bash
 
+echo -e "Installing Reflector...\n"
 sudo pacman -S reflector
+echo -e "Done!\nCreating backup of old mirrorlist...\n"
 sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
+echo -e "Done!\nUpdating mirrorlist with the fastest ones...\n"
 sudo reflector --latest 10 --age 12 --country 'Italy' --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+echo -e "Done!\nSyncing pacman database...\n"
 sudo pacman -Syy
 
-sudo pacman --needed -S base-devel git networkmanager openssh pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber intel-ucode udiskie udisks2 mesa vulkan-intel tlp bluez bluez-utils playerctl
+echo -e "Done!\nInstalling essential packages and services...\n"
+sudo pacman -S base-devel bluez bluez-utils cups cups-pdf git intel-ucode mesa networkmanager openssh pipewire pipewire-pulse pipewire-alsa pipewire-jack playerctl udiskie udisks2 vulkan-intel wireplumber
 
+echo -e "Done!\nEnabling and starting essential services...\n"
 sudo systemctl enable --now sshd
 sudo systemctl enable --now bluetooth
-sudo systemctl enable --now tlp
 sudo systemctl enable --now NetworkManager
+sudo systemctl enable --now cups.service
 
+echo -e "Done!\nCloning and installing yay AUR helper...\n"
 cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ..
+echo -e "Done!\nTidying up yay install...\n"
 rm -rf yay*
 
-sudo pacman --needed -S 7zip brightnessctl btop cifs-utils cliphist cups cups-pdf curl discord epson-inkjet-printer-escpr2 evince fastfetch firefox foot gimp imagemagick jdk21-openjdk jq lua mpv neovim npm nwg-look obs-studio obsidian onefetch pacman-contrib parted pavucontrol playerctl python samba scrot slurp starship steam stow stylua tailscale tar thunar ttf-terminus-nerd unrar unzip virtualbox virtualbox-host-modules-arch waybar wine wget wl-clipboard wlsunset zip
+echo -e "Done!\nDownloading all necessary packages from pacman...\n"
+sudo pacman -S 7zip brightnessctl btop cifs-utils cliphist curl discord evince fastfetch foot fuzzel gimp grim imagemagick jdk21-openjdk jq lua mpv neovim npm nwg-look obs-studio obsidian onefetch pacman-contrib parted pavucontrol playerctl python rustup samba scrot slurp starship steam stow stylua swaybg swaylock swaync tailscale tar telegram-desktop thunar ttf-terminus-nerd unrar unzip virtualbox virtualbox-host-modules-arch waybar wine wget wl-clipboard wlsunset zip
 
-yay -S arc-darkest-theme-git bottles drawio obs-vkcapture papirus-icon-theme surfshark-client tf-devicons ttf-font-awesome ttf-ioskeley-mono vimix-cursors
+echo -e "Done!\nDownloading all necessary packages from yay...\n"
+yay -S arc-darkest-theme-git bottles drawio-desktop-bin epson-inkjet-printer-escpr2 librewolf-bin obs-vkcapture papirus-icon-theme surfshark-client surfshark-vpn-cli-bin ttf-devicons ttf-font-awesome ttf-ioskeley-mono vimix-cursors
 
-sudo systemctl enable --now tailscaled.service
-sudo systemctl enable --now cups.service
+echo -e "Done!\nDownloading rust, rustc and cargo...\n"
+rustup default stable
 
+echo -e "Done!\nStowing dotfiles in the right directories...\n"
 mkdir ~/.dotfiles
-git clone git@github.com:eliaaile9/dotfiles.git
-cd dotfiles/t480-labwc
+cp -r ~/dotfiles/t480-labwc/* ~/.dotfiles
+cd ~/.dotfiles
+rm instructions.md setup.sh
 for app in $(ls -d */ | sed 's/\///'); do
-    if [[ "$app" == ".git" ]]; then continue; fi
-    stow -vRT "$HOME" "$app"
+    stow -v "$app"
+done
+
+echo -e "Done!\nMaking executable all the script files...\n"
+sudo chmod +x ~/.dotfiles/labwc/.config/labwc/scripts/*
+
+echo -e "Done!\nTidying up pacman...\n"
+sudo pacman -R qutebrowser
+sudo pacman -Rns $(pacman -Qdtq)
+sudo pacman -Sc
+yay -Sc
+echo -e "Done\n"
