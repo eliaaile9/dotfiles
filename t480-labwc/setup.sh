6731d56bfafd938454671f32be2ab1e74d55a4e9
eliaaile <eliacortesi@etik.com>
2026-03-11 23:35:22 +0100

    feat: add `zen-browser-bin` package in `setup.sh`
    Add `zen-browser-bin` package in yay section in `setup.sh`

--- t480-labwc/setup.sh
+++ t480-labwc/setup.sh
@@ -31,7 +31,7 @@ echo -e "Done!\nDownloading all necessary packages from pacman...\n"
 sudo pacman -S 7zip brightnessctl btop cifs-utils cliphist curl discord evince fastfetch foot fuzzel gimp grim imagemagick jdk21-openjdk jq lua mpv neovim npm nwg-look obs-studio obsidian onefetch pacman-contrib parted pavucontrol playerctl python rustup samba scrot slurp starship steam stow stylua swaybg swaylock swaync tailscale tar telegram-desktop thunar ttf-terminus-nerd unrar unzip virtualbox virtualbox-host-modules-arch waybar wine wget wl-clipboard wlsunset zip
 
 echo -e "Done!\nDownloading all necessary packages from yay...\n"
-yay -S arc-darkest-theme-git bottles drawio-desktop-bin epson-inkjet-printer-escpr2 librewolf-bin obs-vkcapture papirus-icon-theme surfshark-client surfshark-vpn-cli-bin ttf-devicons ttf-font-awesome ttf-ioskeley-mono vimix-cursors
+yay -S arc-darkest-theme-git bottles drawio-desktop-bin epson-inkjet-printer-escpr2 librewolf-bin obs-vkcapture papirus-icon-theme surfshark-client surfshark-vpn-cli-bin ttf-devicons ttf-font-awesome ttf-ioskeley-mono vimix-cursors zen-browser-bin
 
 echo -e "Done!\nDownloading rust, rustc and cargo...\n"
 rustup default stable
