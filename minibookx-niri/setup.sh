696797adf2a50e95825c63a408b9b8a33833b8d9
eliaaile9 <eliacortesi@etik.com>
2026-06-06 12:24:38 +0200

    feat: add LaTeX relative packages and PDF reader Zathura
    Add the following packages: `texlve-basic`, `texlive-binextra`,
    `texlive-latex`, `texlive-latexrecommended`, `texlive-plaingeneric`,
    `texlive-latexextra`, 'texlive-bibtexextra', 'texlive-fontsrecommended',
    'texlive-pictures', `texlive-mathscience`, `texlive-publishers`,
    `texlive-luatex`, `zathura` in `setup.sh` file

--- minibookx-niri/setup.sh
+++ minibookx-niri/setup.sh
@@ -5,7 +5,7 @@ sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
 sudo reflector --latest 10 --age 12 --country 'Italy' --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Syy
 
-sudo pacman --needed -S base-devel git networkmanager openssh pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber intel-ucode udisksie udisks2 mesa vulkan-intel tlp bluez bluez-utils playerctl
+sudo pacman -S base-devel git networkmanager openssh pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber intel-ucode udisksie udisks2 mesa vulkan-intel tlp bluez bluez-utils playerctl
 
 sudo systemctl enable --now sshd
 sudo systemctl enable --now bluetooth
@@ -19,7 +19,7 @@ makepkg -si
 cd ..
 rm -rf yay*
 
-sudo pacman --needed -S 7zip brightnessctl btop cifs-utils cliphist cups cups-pdf curl epson-inkjet-printer-escpr2 evince fastfetch foot fuzzel grim iio-sensor-proxy imagemagick jdk21-openjdk jq lua mpv neovim npm nwg-look obsidian onefetch pacman-contrib pavucontrol pcmanfm poppler python ripgrep samba scrot  slurp starship stow tailscale tar thunar tlp tmux ttf-iosevka-nerd ttf-terminus-nerd unrar unzip wget wl-clipboard yazi zip zoxide
+sudo pacman -S 7zip brightnessctl btop cifs-utils cliphist cups cups-pdf curl epson-inkjet-printer-escpr2 evince fastfetch foot fuzzel grim iio-sensor-proxy imagemagick jdk21-openjdk jq lua mpv neovim npm nwg-look obsidian onefetch pacman-contrib pavucontrol pcmanfm poppler python ripgrep samba scrot  slurp starship stow tailscale tar texlive-basic texlive-binextra texlive-latex texlive-latexrecommended texlive-plaingeneric texlive-latexextra texlive-bibtexextra texlive-fontsrecommended texlive-pictures texlive-mathscience thunar tlp tmux ttf-iosevka-nerd ttf-terminus-nerd unrar unzip wget wl-clipboard yazi zip zoxide
 
 yay -S awww-bin graphite-gtk-theme-black-git librewolf-bin rot8-git surfshark-client surfshark-vpn-cli-bin suru-plus-dark-git ttf-devicons ttf-font-awesome vimix-cursors
 
